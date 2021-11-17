5c9a726262e03f3c81242561e966f5a4d394045f
John K <john.kellyfitness@gmail.com>
2021-11-17 07:47:16 -0500

    semi-complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,13 +11,13 @@ import Header from "./Header";
 function App() {
   return (
     <div className="App">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <header>
      <Navigation/>
      </header>
      <main>
-     
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <Header/>
+     </meta>
      <br />
      <div className="bg-dark">
      <br/>
@@ -26,7 +26,7 @@ function App() {
      <Contacts />
       </div>
      </main>
-     </meta>
+    
      </div>
      
   );
