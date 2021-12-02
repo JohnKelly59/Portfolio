fa8d9f6d94729de425b3dabd30b4fe357018293d
John K <john.kellyfitness@gmail.com>
2021-12-02 16:42:34 -0500

    added s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import {Navbar, Nav, Container} from "react-bootstrap";
-import Fade from 'react-reveal/Fade';
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Fade from "react-reveal/Fade";
 
-function Navigation(){
+function Navigation() {
+  return (
+    <>
+      <Fade>
+        <Navbar fixed="top" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#">John Kelly</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#languages">Languages</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </Fade>
+    </>
+  );
+}
 
-return(
-  <>
-  <Fade>
-  <Navbar fixed="top" bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#">John Kelly</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#projects">Project</Nav.Link>
-      <Nav.Link href="#languages">Languages</Nav.Link>
-      <Nav.Link href="#contact">Contact</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-  </Fade>
-  </>
-  )};
-  
-  export default Navigation;
+export default Navigation;
