204151863362953a56e317632e739034479c3ccb
John K <john.kellyfitness@gmail.com>
2022-01-19 21:22:02 -0500

    collapse

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import Fade from "react-reveal/Fade";
 import Resume from "./resume.pdf";
 
@@ -7,21 +7,23 @@ function Navigation() {
   return (
     <Fade>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home"> John Kelly</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#languages">Stack</Nav.Link>
-            <Nav.Link href="#About">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href={Resume} download>
-              Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Container>
+          <Navbar.Brand href="#home"> John Kelly</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#languages">Stack</Nav.Link>
+              <Nav.Link href="#About">About</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href={Resume} download>
+                Resume
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </Fade>
   );
