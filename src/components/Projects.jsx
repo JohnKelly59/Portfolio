1c34047e044a7a25db419bfdb96b18b6590ac36a
John K <john.kellyfitness@gmail.com>
2022-01-21 22:48:03 -0500

    changed time description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -212,13 +212,13 @@ function Project() {
                 HTML, CSS, JavaScript, Node.js, APIs, AWS, EJS
               </h6>
               <p className="lead text-light">
-                Current Weather App This simple web application gets the city
-                and state that the user inputs into the page and sends a request
-                to the "openweathermap" API. The API then returns a JSON package
-                which holds information about the requested location's weather.
-                The information recieved is then parsed and displayed to the
-                user. A contact page is also put in place so that the user can
-                ask me any questions via email.
+                Current Weather Web application gets the city and state that the
+                user inputs into the page and sends a request to the
+                "openweathermap" API. The API then returns a JSON package which
+                holds information about the requested location's weather. The
+                information recieved is then parsed and displayed to the user. A
+                contact page is also put in place so that the user can ask me
+                any questions via email.
               </p>
             </div>
             <div className="col-md-5 order-md-1">
