95588648f658c0f3992082e6e7e135ad2b5021ee
John K <john.kellyfitness@gmail.com>
2022-01-25 15:25:32 -0500

    changed navbar:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,11 +17,6 @@ function Navigation() {
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
-            <Nav>
-              <Nav.Link href={Resume} download>
-                Resume
-              </Nav.Link>
-            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
